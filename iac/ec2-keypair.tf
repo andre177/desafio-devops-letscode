f62e1afd272806a772573e3612c85d0e225659da
Andre Ferreira <andreferreira177@gmail.com>
2022-12-08 23:34:29 -0300

    feature: adding bastion host

---
+++ iac/ec2-keypair.tf
@@ -0,0 +1,9 @@
+module "key_pair" {
+  source             = "terraform-aws-modules/key-pair/aws"
+  key_name           = "default-key"
+  create_private_key = true
+}
+
+output "key_pair_private_pem" {
+  value = module.key_pair.private_key_pem
+}
