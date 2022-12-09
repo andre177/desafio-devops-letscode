4235a3dcc550709fd9108219345332de7beaee6b
Andre Ferreira <andreferreira177@gmail.com>
2022-12-09 00:18:35 -0300

    chore: testing again

--- iac/ec2-instance-profile.tf
+++ iac/ec2-instance-profile.tf
@@ -4,6 +4,7 @@ resource "aws_iam_instance_profile" "k8s_master_instance_profile" {
 }
 
 resource "aws_iam_instance_profile" "k8s_node_instance_profile" {
+  #test
   name = "K8sNodeInstanceProfile"
   role = aws_iam_role.k8s_node.name
 }
