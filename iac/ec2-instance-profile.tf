f916db13be9ccfd3f1a64c4ba3237ad0c806a1b3
Andre Ferreira <andreferreira177@gmail.com>
2022-12-09 00:11:16 -0300

    feature: adding worker node for k8s

--- iac/ec2-instance-profile.tf
+++ iac/ec2-instance-profile.tf
@@ -2,3 +2,8 @@ resource "aws_iam_instance_profile" "k8s_master_instance_profile" {
   name = "K8sMasterInstanceProfile"
   role = aws_iam_role.k8s_master.name
 }
+
+resource "aws_iam_instance_profile" "k8s_node_instance_profile" {
+  name = "K8sNodeInstanceProfile"
+  role = aws_iam_role.k8s_node.name
+}
