44301e855a6c374f5cbc9df49ae5029b10816b80
Andre Ferreira <andreferreira177@gmail.com>
2022-12-15 22:24:02 -0300

    fix: no configmap needed for frontend-app

--- private-iac/frontend-app.tf
+++ private-iac/frontend-app.tf
@@ -1,40 +1,35 @@
-resource "kubernetes_deployment" "backend_app" {
+resource "kubernetes_deployment" "frontend_app" {
   metadata {
-    name      = local.app_name
-    namespace = local.namespace
+    name      = local.frontend_app_name
+    namespace = var.ada_namespace
   }
   spec {
     replicas = 1
     selector {
       match_labels = {
-        "app.kubernetes.io/name" = local.app_name
-        "app"                    = local.app_name
+        "app.kubernetes.io/name" = local.frontend_app_name
+        "app"                    = local.frontend_app_name
       }
     }
     template {
       metadata {
         labels = {
-          "app.kubernetes.io/name" = local.app_name
-          "app"                    = local.app_name
+          "app.kubernetes.io/name" = local.frontend_app_name
+          "app"                    = local.frontend_app_name
         }
       }
       spec {
         container {
-          image             = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com/${local.app_name}:latest"
-          name              = local.app_name
+          image             = "${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com/${local.frontend_app_name}:latest"
+          name              = local.frontend_app_name
           image_pull_policy = "Always"
 
           liveness_probe {
             http_get {
-              path = "/actuator/health"
-              port = local.app_port
+              path = "/login"
+              port = local.frontend_app_port
             }
 
-          }
-          env_from {
-            config_map_ref {
-              name = local.app_name
-            }
           }
         }
         image_pull_secrets {
@@ -45,29 +40,28 @@ resource "kubernetes_deployment" "backend_app" {
   }
 }
 
-resource "kubernetes_service" "backend_app" {
+resource "kubernetes_service" "frontend_app" {
   metadata {
-    name      = local.app_name
-    namespace = local.namespace
+    name      = local.frontend_app_name
+    namespace = var.ada_namespace
   }
   spec {
     selector = {
-      app = local.app_name
+      app = local.frontend_app_name
     }
     port {
       port        = 80
-      target_port = local.app_port
+      target_port = local.frontend_app_port
     }
     type = "LoadBalancer"
   }
 }
 
-output "load_balancer_hostname" {
-  value = kubernetes_service.backend_app.status.0.load_balancer.0.ingress.0.hostname
+output "frontendapp_load_balancer_hostname" {
+  value = kubernetes_service.frontend_app.status.0.load_balancer.0.ingress.0.hostname
 }
 
 locals {
-  app_name  = "backend-app"
-  namespace = "ada-apps"
-  app_port  = 8080
+  frontend_app_name = "frontend-app"
+  frontend_app_port = 4200
 }
