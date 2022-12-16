c4be2dbcbca943d33e174529fefad43c9860614c
Andre Ferreira <andreferreira177@gmail.com>
2022-12-16 00:59:21 -0300

    hotfix: forcing deleting ecr repos when destroying

--- iac/ecr.tf
+++ iac/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "backend_app" {
   name                 = "backend-app"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -10,6 +11,7 @@ resource "aws_ecr_repository" "backend_app" {
 resource "aws_ecr_repository" "frontend_app" {
   name                 = "frontend-app"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
